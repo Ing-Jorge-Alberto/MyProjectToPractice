f5a19fa377dd5dd65a43ce615aa1c1399dfa502f
GeorgeMac <100180594+Ing-Jorge-Alberto@users.noreply.github.com>
2022-10-25 21:13:34 -0500

    Primer commit
    Agrego un sysout para hacer mi primer commit.

--- MyProjectToPractice/src/HelloJac.java
+++ MyProjectToPractice/src/HelloJac.java
@@ -1,5 +1,6 @@
 public class HelloJac {
     public static void main(String[] args) {
         System.out.println("Welcome back to JAVA Jac");
+        System.out.println("Cambio para usar Git: commit y GitHub Desktop");
     }
 }
