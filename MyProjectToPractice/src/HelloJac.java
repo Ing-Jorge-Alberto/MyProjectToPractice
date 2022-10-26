2cddc660ef0c1280c35637ab5846d46f3defcbb5
JACLinuxMint <ing_jorge_alberto__canales@hotmail.com>
2022-10-26 00:57:25 -0500

    Cambios desde linux

--- MyProjectToPractice/src/HelloJac.java
+++ MyProjectToPractice/src/HelloJac.java
@@ -2,5 +2,26 @@ public class HelloJac {
     public static void main(String[] args) {
         System.out.println("Welcome back to JAVA Jac");
         System.out.println("Cambio para usar Git: commit y GitHub Desktop");
+
+        System.out.println("Primitive Data Types");
+        System.out.println("Integer types");
+
+        byte myByte=50;
+        short myShort=32767;
+        int myInt=2147483647;
+        long myLong=9223372036854775807L;
+        float myFloat=3.14f;
+        double myDouble=3.14;
+        boolean myBoolean=true;
+        char myChar='A';
+        System.out.println("byte: "+myByte);
+        System.out.println("short: "+myShort);
+        System.out.println("int: "+myInt);
+        System.out.println("long: "+myLong);
+
+        System.out.println("float: "+myFloat);
+        System.out.println("double: "+myDouble);
+        System.out.println("boolean: "+myBoolean);
+        System.out.println("char: "+myChar);
     }
 }
