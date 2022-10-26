8498947bc026a5ed419baaaa77bfd68f5a8403b2
GeorgeMac <100180594+Ing-Jorge-Alberto@users.noreply.github.com>
2022-10-26 11:21:25 -0500

    Se modifica orden de impresion

--- MyProjectToPractice/src/HelloJac.java
+++ MyProjectToPractice/src/HelloJac.java
@@ -2,10 +2,9 @@ public class HelloJac {
     public static void main(String[] args) {
         System.out.println("Welcome back to JAVA Jac");
         System.out.println("Cambio para usar Git: commit y GitHub Desktop");
-
+        System.out.println("\n");
         System.out.println("Primitive Data Types");
         System.out.println("Integer types");
-
         byte myByte=50;
         short myShort=32767;
         int myInt=2147483647;
@@ -18,10 +17,11 @@ public class HelloJac {
         System.out.println("short: "+myShort);
         System.out.println("int: "+myInt);
         System.out.println("long: "+myLong);
-
-        System.out.println("float: "+myFloat);
-        System.out.println("double: "+myDouble);
         System.out.println("boolean: "+myBoolean);
         System.out.println("char: "+myChar);
+        System.out.println("\n");
+        System.out.println("Float types");
+        System.out.println("float: "+myFloat);
+        System.out.println("double: "+myDouble);
     }
 }
