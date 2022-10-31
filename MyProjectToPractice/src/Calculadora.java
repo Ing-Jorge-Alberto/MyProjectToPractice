785aad70723a17e7ce918b7d9128861f976938c5
GeorgeMac <100180594+Ing-Jorge-Alberto@users.noreply.github.com>
2022-10-31 01:01:53 -0600

    Se agregan las clases Calculadora y Suma.

---
+++ MyProjectToPractice/src/Calculadora.java
@@ -0,0 +1,7 @@
+public class Calculadora {
+    public static void main(String[] args) {
+        Suma suma = new Suma();
+        //suma.Sumar(2,3);
+        System.out.println(suma.Sumar(2,3));
+    }
+}
---
+++ MyProjectToPractice/src/Suma.java
@@ -0,0 +1,8 @@
+public class Suma{
+    private int resultado;
+
+    public int Sumar(int numero1, int numero2){
+        resultado = numero1 + numero2;
+        return resultado;
+    }
+}
