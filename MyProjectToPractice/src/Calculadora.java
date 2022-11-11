299ee7f7b3ba05c2cae6bd33c3d6cec0eacfbda8
GeorgeMac <100180594+Ing-Jorge-Alberto@users.noreply.github.com>
2022-11-11 01:12:26 -0600

    Switch
    Ejemplo de Switch - Case utilizando la clase Suma en el Main

--- MyProjectToPractice/src/Calculadora.java
+++ MyProjectToPractice/src/Calculadora.java
@@ -1,7 +1,23 @@
 public class Calculadora {
     public static void main(String[] args) {
-        Suma suma = new Suma();
-        //suma.Sumar(2,3);
-        System.out.println(suma.Sumar(2,3));
+        int option = 1;
+
+        System.out.println("+++ Calculadora +++");
+        System.out.println("1.- Suma");
+        System.out.println("2.- Resta - No disponible");
+        System.out.println("3.- Multiplicacion - No disponible");
+        System.out.println("4.- Division - No disponible");
+        System.out.println("\n");
+
+        switch (option){
+            case 1:
+                Suma suma = new Suma();
+                //suma.Sumar(2,3);
+                System.out.println("La suma de 2+3 es: "+suma.Sumar(2,3));
+                break;
+
+            default:
+                System.out.println("Opcion "+option+" no valida");
+        }
     }
 }
